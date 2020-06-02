cf45684c32160e6ff0cef7c104ba9fe724ed2053
saeinlee <>
2020-06-02 17:49:51 +0900

    react - node server 구축

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/api/test", function (req, res) {
+    res.send({
+        greeting: "Hello React X Node.js"
+    });
+});
+
+module.exports = router
