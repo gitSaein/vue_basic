319091a0db20db19871290e27de33b97688c7d35
saeinlee <>
2020-06-03 13:59:07 +0900

    config posgresql

--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const app = express()
-const api = require('./routes/index')
+const express = require("express");
+const { Client } = require("pg");
+const bodyParser = require("body-parser");
+const app = express();
+const api = require("./routes/index");
 const port = process.env.PORT || 5000;
+//https://node-postgres.com/api/client
+const client = new Client({
+  host: "localhost",
+  port: 5432,
+  user: "admin",
+  password: "admin",
+});
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("connected");
+  }
+});
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use("/api", api)
+app.use("/api", api);
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
